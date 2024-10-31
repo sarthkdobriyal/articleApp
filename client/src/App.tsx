de2b2f87917fbdb09c748f255d72d27a246c8278
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2024-10-31 14:21:30 +0530

    initial commit with authentication

---
+++ client/src/App.tsx
@@ -0,0 +1,51 @@
+// src/App.tsx
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import Auth from "./components/Auth";
+import Dashboard from "./pages/Dashboard";
+import { Toaster } from 'sonner';
+
+const queryClient = new QueryClient();
+
+const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { token, isLoading } = useAuth();
+  if (isLoading) return <div>Loading...</div>;
+  return token ? <>{children}</> : <Navigate to="/login" />;
+};
+
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AuthProvider>
+          <div className="">
+            <Toaster position="top-right" />
+            <Routes>
+              <Route path="/login" element={<Auth />} />
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              
+            </Routes>
+          </div>
+        </AuthProvider>
+      </Router>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+import authRoutes from './routes/auth';
+
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+
+app.use(cors({origin: `${process.env.FRONT_URL}`, credentials: true}))
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export { prisma };
